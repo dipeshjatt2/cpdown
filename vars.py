3f7e319c8e70f9f682b87112cdf16fbbda7a9cf9
dipeshjatt2 <101973614+dipeshjatt2@users.noreply.github.com>
2024-08-22 20:36:55 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,6 +1,6 @@
 api_id = 22118129 #change_with_your_Api_id.
 api_hash = "43c66e3314921552d9330a4b05b18800" #change_with_your_Api_hash.
-bot_token = "7198600769:AAHfVi7qTaMw20HbpvPrF-fnP77-sWdOthI" #change_with_your_bot_token.
+bot_token = "7384767506:AAElEB0m8ZRvIj8h-WkgeETPghwyNfzcPz8" #change_with_your_bot_token.
 auth_users = [999741495] #change_with_your_user_id.
 sudo_user = 1713924419 #change_with_your_user_id.
 log_channel = -1002154281532 #change_with_your_channel_id.
