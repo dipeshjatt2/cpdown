eb018d6c453c875b98a9191d2c3bc827ffe5138d
dipeshjatt2 <101973614+dipeshjatt2@users.noreply.github.com>
2024-08-22 20:34:47 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,4 +1,4 @@
-api_id = 35814071 #change_with_your_Api_id.
+api_id = 22118129 #change_with_your_Api_id.
 api_hash = "43c66e3314921552d9330a4b05b18800" #change_with_your_Api_hash.
 bot_token = "7198600769:AAHfVi7qTaMw20HbpvPrF-fnP77-sWdOthI" #change_with_your_bot_token.
 auth_users = [999741495] #change_with_your_user_id.
